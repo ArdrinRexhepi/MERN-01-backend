216efd988795847fbcfa96e4b3d865863c0be174
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2023-08-12 01:48:25 +0200

    assigned workouts to users

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -2,7 +2,10 @@ const Workout = require("../models/workoutModel");
 const mongoose = require("mongoose");
 //get all workouts
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  const user_id = req.user._id;
+  const workouts = await Workout.find({ user_id }).sort({
+    createdAt: -1,
+  });
 
   res.status(200).json(workouts);
 };
@@ -44,7 +47,10 @@ const createWorkout = async (req, res) => {
   }
   //   add doc to db
   try {
-    const workout = await Workout.create({ title, reps, load });
+    const user_id = req.user._id;
+
+    console.log("user_id ==>", user_id);
+    const workout = await Workout.create({ title, reps, load, user_id });
     res.status(200).json(workout);
   } catch (error) {
     res.status(400).json({ error: error.message });
